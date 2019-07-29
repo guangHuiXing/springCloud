f75b17d37d2c76825e9f5e96667c58c38f424deb
xinggh <1qaz!QAZ>
2019-07-29 14:14:33 +0800

    将feign 熔断器设置为实现fallbackfactory

--- service-feign/src/main/java/com/xgh/servicefeign/api/SchedualServiceHiHystric.java
+++ service-feign/src/main/java/com/xgh/servicefeign/api/SchedualServiceHiHystric.java
@@ -1,14 +1,26 @@
 package com.xgh.servicefeign.api;
 
+import feign.hystrix.FallbackFactory;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by XGH on 2019/7/28
  */
 @Component
-public class SchedualServiceHiHystric implements SchedualServiceHi {
-    @Override
+public class SchedualServiceHiHystric /*implements SchedualServiceHi */ implements FallbackFactory<SchedualServiceHi> {
+   /* @Override
     public String sayHiFromClientOne(String name) {
         return "sorry" + name;
+    }*/
+
+   //不直接实现接口，而是实现Fallrollback
+    @Override
+    public SchedualServiceHi create(Throwable throwable) {
+        return new SchedualServiceHi(){
+            @Override
+            public String sayHiFromClientOne(String name) {
+               return "sorry" + name;
+            }
+        };
     }
 }
